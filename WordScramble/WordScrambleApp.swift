764c2e9639287410792a9e6918c3ab26eeecfc42
LuiArm <Luisarmendariz24@icloud.com>
2023-04-21 22:27:01 -0600

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by luis armendariz on 4/21/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
